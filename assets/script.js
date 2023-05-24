cbf770946ae0025aea53e2273dcfcf36690528d7
Christina2208 <christinalf22@gmail.com>
2023-05-24 14:36:35 -0400

    created varibles

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+let city = document.getElementById("city");
+let searchBtn = document.getElementById("searchBtn");
+let displayCity = document.getElementById("displayCity");
+let date = document.getElementById("date");
+let icon = document.getElementsByClassName("icon");
+let temp = document.getElementById("temp");
+let description = document.getElementById("description");
+let wind = document.getElementById("wind");
+let humidity = document.getElementById("humidity");
+let historyItem = document.getElementById("historyItem");
+
